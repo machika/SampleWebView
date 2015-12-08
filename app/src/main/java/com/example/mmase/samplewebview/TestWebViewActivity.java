0588dd40874bb374cdb39740d5afa9827a54533d
mmase <machi0545@hotmail.com>
2015-12-08 15:52:52 +0900

    URL bar and Go button

--- app/src/main/java/com/example/mmase/samplewebview/TestWebViewActivity.java
+++ app/src/main/java/com/example/mmase/samplewebview/TestWebViewActivity.java
@@ -2,9 +2,12 @@ package com.example.mmase.samplewebview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class TestWebViewActivity extends Activity {
+public class TestWebViewActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,15 @@ public class TestWebViewActivity extends Activity {
         setContentView(R.layout.activity_test_web_view);
         WebView myWebView = (WebView)findViewById(R.id.mywebview);
         myWebView.loadUrl("http://www.yahoo.co.jp");
+
+        findViewById(R.id.go_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText myEditText = (EditText)(findViewById(R.id.urlbar));
+        String urlToGo = myEditText.getText().toString();
+        WebView myWebView = (WebView)findViewById(R.id.mywebview);
+        myWebView.loadUrl(urlToGo);
     }
 }
